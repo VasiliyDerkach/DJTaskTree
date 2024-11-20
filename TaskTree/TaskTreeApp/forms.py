ffe38da36dcd94d51c89a6c56c82963213f1ba72
Vasiliy <v_derkach@inbox.ru>
2024-11-20 20:02:39 +0500

    2011

--- TaskTree/TaskTreeApp/forms.py
+++ TaskTree/TaskTreeApp/forms.py
@@ -3,18 +3,31 @@
  class CreateContact - форма для создания нового контакта
     last_name -фамилия (обязательное поле)
     first_name - имя
-    second_name - отяество
+    second_name - отчество
 class CreateTask - форма для создания новой задачи
     title -  текст заголовка задачи (обязательное поле)
     start - дата старта задачи
     end - дата завершения задачи
 """
 from django import forms
+"""
+class CreateContact - форма для создания нового контакта
+    last_name -фамилия (обязательное поле)
+    first_name - имя
+    second_name - отчество
+"""
+
 class CreateContact(forms.Form):
     last_name = forms.CharField(max_length=75, label='Введите фамилию')
     first_name = forms.CharField(max_length=75,label='Введите имя')
     second_name = forms.CharField(max_length=75, label='Введите отяество')
 
+"""
+class CreateTask - форма для создания новой задачи
+    title -  текст заголовка задачи (обязательное поле)
+    start - дата старта задачи
+    end - дата завершения задачи
+"""
 class CreateTask(forms.Form):
     title = forms.CharField(max_length=250, label='Введите текст задачи')
     start = forms.DateField(label='Введите дату старта', required=False)
